6481cd2be336214661196ce6bb260555e610a6e9
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-07-28 19:47:02 -0400

    get route set up

--- database/index.js
+++ database/index.js
@@ -1,1 +1,76 @@
+require("dotenv").config();
+
+// process.env.MONGODB_URI,
+// {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }
 const mongoose = require('mongoose');
+
+mongoose.connect(
+  `mongodb://localhost:27017/${process.env.DB_NAME}`
+);
+
+let questionSchema = mongoose.Schema ({
+id: {type: Number, required: true},
+product_id: {type: Number, required: true},
+body: {type: String, required: true},
+date_written: {type: Date, required: true},
+asker_name: {type: String, required: true},
+asker_email: {type: String, required: true},
+reported: {type: Boolean, default: false},
+helpful: {type: Number, default: 0}
+})
+
+
+// {
+//   "_id": {
+//     "$oid": "62e2813f06f44dc6c287317f"
+//   },
+//   "id": 1,
+//   "question_id": 36,
+//   "body": "Supposedly suede, but I think its synthetic",
+//   "date_written": 1599958385988,
+//   "answerer_name": "sillyguy",
+//   "answerer_email": "first.last@gmail.com",
+//   "reported": 0,
+//   "helpful": 1
+// }
+let answerSchema = mongoose.Schema({
+id: {type: Number, required: true},
+question_id: {type: Number, required: true},
+body: {type: String, required: true},
+date_written: {type: Date, required: true},
+answerer_name: {type: String, required: true},
+answerer_email: {type: String, required: true},
+reported: {type: Boolean, default: false},
+helpful: {type: Number, default: 0}
+})
+
+let Question = mongoose.model('Question', questionSchema );
+let Answer = mongoose.model('Answer', answerSchema);
+
+let findAll = function(cb) {
+  Question.find({}, function(err, questions) {
+    if (err) {
+      console.log('Err when finding questions', err)
+    } else {
+      cb(questions)
+    }
+  }).limit(50);
+
+
+  // Answer.find({}, function(err, answers) {
+  //   if (err) {
+  //     console.log('Err when finding questions', err)
+  //   } else {
+  //     cb(answers)
+  //   }
+  // }).limit(50).catch((err) => {console.log('error in answer find func:', error)})
+}
+
+
+
+
+
+module.exports.findAll = findAll;
--- server/index.js
+++ server/index.js
@@ -1,2 +1,26 @@
+
+require("dotenv").config();
+
+const {findAll} = require('../database/index.js')
 const express = require('express');
+
 let app = express();
+
+
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}!`));
+
+app.use(express.json());
+
+//qa/questions/?product_id=${productId}
+app.get('/', (req, res) => {
+
+
+
+
+    findAll((questions) => {
+      console.log('inside findAll in app', questions)
+
+      res.send(questions);
+    })
+
+});
