7a62499ae92b029911b84c941179c6e0cc00db67
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-07-29 14:54:36 -0400

    added page and count functionality

--- server/index.js
+++ server/index.js
@@ -13,10 +13,20 @@ app.use(express.json());
 
 //getting all answers for a specific question
 app.get('/qa/questions/:question_id/answers', (req, res) => {
-     console.log(req.params)
-
-
-    Answer.find(req.params).limit(50).then((answers) => {
+     console.log('answer get request', req.params)
+      let page = 1;
+     if (req.params.page <= 0) {
+       page = 1
+    } else {
+       page = req.params.page
+    }
+      let count = 5;
+    if (req.params.count <= 0) {
+       count = 5
+    } else {
+      count = req.params.count
+    }
+    Answer.find({question_id: req.params.question_id}).limit(Number(count)).skip(Number(page)).then((answers) => {
       console.log(answers)
     res.send(answers)
    }).catch((error) => {
@@ -26,9 +36,23 @@ app.get('/qa/questions/:question_id/answers', (req, res) => {
 });
 
 //getting all questions for a specific product
-app.get(`/qa/questions/:product_id`, (req, res) => {
+app.get(`/qa/questions/:product_id&count=:count&page=:page`, (req, res) => {
+
+  let page;
+  if (req.params.page <= 0) {
+     page = 1
+  } else {
+     page = req.params.page
+  }
+  let count;
+  if (req.params.count <= 0) {
+    count = 5
+  } else {
+    count = req.params.count
+  }
+
   console.log(req.params)
-   Question.find(req.params).limit(50).then((questions) =>{
+   Question.find({product_id: req.params.product_id}).limit(50).then((questions) =>{
     for (var i = 0; i < questions.length; i++) {
       console.log(questions[i].id)
       let id = questions[i].id;
@@ -43,7 +67,7 @@ app.get(`/qa/questions/:product_id`, (req, res) => {
 })
 
 //I want a route that gives me all Q&As for a specific product
-app.get(`/qa/questions/`, (req, res) => {
+app.get(`/qa/questions/&count=:count&page=:page`, (req, res) => {
   console.log(req.params)
    Question.find({}).limit(50).then((questions) =>{
     console.log(questions);
