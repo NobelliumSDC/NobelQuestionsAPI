f43defbc1436ca33b78453c01b6473b9036a6fe3
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-07-31 13:47:01 -0400

    PUT req working.

--- database/index.js
+++ database/index.js
@@ -1,19 +1,72 @@
 require("dotenv").config();
+const path = require('path');
 
 
 const mongoose = require('mongoose');
+const { MongoClient } = require("mongodb");
+////const client = new MongoClient( `mongodb://localhost:27017/${process.env.DB_NAME}`);
+
+// async function run() {
+//   try {
+//     // Connect the client to the server (optional starting in v4.7)
+//     await client.connect();
+//     // Establish and verify connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Connected successfully to server");
+
+
+//     const database = client.db("qa");
+//     const questions = database.collection("questions");
+//     // Query for a movie that has the title 'The Room'
+//     const query = { "product_id": 1 };
+//     const options = {
+//       // sort matched documents in descending order by rating
+//       sort: { "helpful": -1 },
+//       // Include only the `title` and `imdb` fields in the returned document
+
+//     };
+//     const question = await questions.findOne(query, options);
+//     // since this method returns the matched document, not a cursor, print it directly
+//     console.log(question);
+
+//    const merged = await database.questions.aggregate([
+//     {
+//             $lookup:
+//               {
+//                 from: "answers",
+//                 localField: "id",
+//                foreignField: "question_id",
+//                 as: "answers"
+//              }
+//           },
+
+//        ]);
+//        console.log(merged)
+//   } finally {
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
 
 mongoose.connect(
   `mongodb://localhost:27017/${process.env.DB_NAME}`
 );
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 let questionSchema = mongoose.Schema ({
-id: {type: Number, required: true},
-product_id: {type: Number, required: true},
-body: {type: String, required: true},
-date_written: {type: Date, required: true},
-asker_name: {type: String, required: true},
-asker_email: {type: String, required: true},
+id: {type: Number},
+product_id: {type: Number},
+body: {type: String},
+date_written: {type: Date},
+asker_name: {type: String},
+asker_email: {type: String},
 reported: {type: Boolean, default: false},
 helpful: {type: Number, default: 0}
 })
@@ -21,16 +74,37 @@ helpful: {type: Number, default: 0}
 
 
 let answerSchema = mongoose.Schema({
-id: {type: Number, required: true},
-question_id: {type: Number, required: true},
-body: {type: String, required: true},
-date_written: {type: Date, required: true},
-answerer_name: {type: String, required: true},
-answerer_email: {type: String, required: true},
+id: {type: Number},
+question_id: {type: Number},
+body: {type: String},
+date_written: {type: Date},
+answerer_name: {type: String},
+answerer_email: {type: String},
 reported: {type: Boolean, default: false},
 helpful: {type: Number, default: 0}
 })
 
+
+let allInfoSchema = mongoose.Schema({
+  id: {type: Number},
+product_id: {type: Number},
+body: {type: String},
+date_written: {type: Date},
+asker_name: {type: String},
+asker_email: {type: String},
+reported: {type: Boolean, default: false},
+helpful: {type: Number, default: 0},
+answers: [{id: Number,
+  question_id: Number,
+  body: String,
+  date_written: Date,
+  answerer_name: String,
+  answerer_email: String,
+  reported: {type: Boolean, default: false},
+  helpful: {type: Number, default: 0},
+   photos: [{id: Number, answer_id: Number, url: String}]}]
+})
+let AllInfo = mongoose.model('AllInfo', allInfoSchema, 'allInfo');
 let Question = mongoose.model('Question', questionSchema );
 let Answer = mongoose.model('Answer', answerSchema);
 
@@ -57,6 +131,8 @@ let Answer = mongoose.model('Answer', answerSchema);
 
 
 
-//module.exports.findAll = findAll;
+
+
+module.exports.AllInfo = AllInfo;
 module.exports.Answer = Answer;
 module.exports.Question = Question;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 
 require("dotenv").config();
 
-const { Answer, Question} = require('../database/index.js')
+const { Answer, Question, AllInfo } = require('../database/index.js')
 const express = require('express');
 
 let app = express();
@@ -11,8 +11,13 @@ app.listen(process.env.PORT, () => console.log(`Server is running on port ${proc
 
 app.use(express.json());
 
-//getting all answers for a specific question
+
+///////////////////////////// ROUTES ///////////////////////////////////
+
+//////// Getting all answers (answer data only) for a specific question ////////
+///////////////////////////////////////////////////////////////////////////////
 app.get('/qa/questions/:question_id/answers', (req, res) => {
+
      console.log('answer get request', req.params)
       let page = 1;
      if (req.params.page <= 0) {
@@ -35,16 +40,18 @@ app.get('/qa/questions/:question_id/answers', (req, res) => {
 
 });
 
-//getting all questions for a specific product
-app.get(`/qa/questions/:product_id&count=:count&page=:page`, (req, res) => {
 
-  let page;
+///// Getting all (questions/answers/photos) for a specific product ////
+///////////////////////////////////////////////////////////////////////
+app.get(`/qa/questions/:product_id`, (req, res) => {
+
+  let page = 1;
   if (req.params.page <= 0) {
      page = 1
   } else {
      page = req.params.page
   }
-  let count;
+  let count = 5;
   if (req.params.count <= 0) {
     count = 5
   } else {
@@ -52,27 +59,60 @@ app.get(`/qa/questions/:product_id&count=:count&page=:page`, (req, res) => {
   }
 
   console.log(req.params)
-   Question.find({product_id: req.params.product_id}).limit(50).then((questions) =>{
-    for (var i = 0; i < questions.length; i++) {
-      console.log(questions[i].id)
-      let id = questions[i].id;
-      Answer.find({question_id: id}).limit(50).then((answers) => {
-        console.log('answers:', answers)
-      })
-    }
-    //res.send(questions)
-   }).catch((error) => {
-    console.log('error:', error)
-   })
+
+ AllInfo.find({product_id: req.params.product_id}).limit(Number(count)).skip(Number(page)).then((data) => {
+  console.log('data', data)
+  res.send(data)
+ })
+
 })
 
-//I want a route that gives me all Q&As for a specific product
-app.get(`/qa/questions/&count=:count&page=:page`, (req, res) => {
-  console.log(req.params)
-   Question.find({}).limit(50).then((questions) =>{
-    console.log(questions);
-    res.send(questions)
-   }).catch((error) => {
-    console.log('error:', error)
-   })
+///////// Updating Helpful Field for Question /////////////
+////////////////////////////////////////////////////////
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  console.log(req.params.question_id)
+  AllInfo.findOneAndUpdate({id: req.params.question_id}, {$inc: {helpful: 1}}, {new: true}).then((data) => {
+    console.log(data)
+      res.sendStatus(200);
+
+  }).catch((err) => {
+    console.log('err', err);
+  });
 })
+
+
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  console.log(req.params);
+})
+
+
+
+//////////////////////////////////////////////
+ //    FOR Merging data via Shell         //
+//////////////////////////////////////////////
+//        db.questions.aggregate([
+//    {
+//      $lookup:
+//        {
+//          from: "answersAndPhotos",
+//          localField: "id",
+//          foreignField: "question_id",
+//          as: "answers"
+//        }
+//    },
+//        {$out : "allInfo" }
+//  ]);
+
+
+//  db.answers.aggregate([
+//   {
+//     $lookup:
+//       {
+//         from: "photos",
+//         localField: "id",
+//         foreignField: "answer_id",
+//         as: "photos"
+//       }
+//   },
+//       {$out : "answersAndPhotos" }
+// ]);
