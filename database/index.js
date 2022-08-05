5fddbc92f6371f65f4cf8f25d17613c6f55b1cb9
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-08-05 10:59:33 -0400

    Please enter the commit message for your changes. Lines starting

--- database/index.js
+++ database/index.js
@@ -10,7 +10,7 @@ const { MongoClient } = require("mongodb");
 
 
 mongoose.connect(
-  `mongodb://${process.env.MONGO_HOST}:27017/${process.env.DB_NAME}`
+  `mongodb://${process.env.MONGODB_HOST}:27017/${process.env.DB_NAME}`
 );
 const db = mongoose.connection;
 
